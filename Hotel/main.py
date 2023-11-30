e5fa7b52420279d7c73672967b986b1542f9a627
RYUKOUKOU <liuhonghao159@gmail.com>
2023-11-30 09:48:23 +0900

    mian

--- Hotel/main.py
+++ Hotel/main.py
@@ -1,5 +1,10 @@
-import flask
+from flask import Flask,render_template,request,url_for
 from system import read_info,save_info
+
+main = Flask(__name__)
+@main.route('/')
+def index():
+    return render_template('index.html')
 
 def text(id):
     data= read_info(id)
