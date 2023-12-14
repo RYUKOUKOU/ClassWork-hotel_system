06570f97c39f3b09a8e1a7e8e9b4dc9e9a8b9f03
RYUKOUKOU <liuhonghao159@gmail.com>
2023-12-14 14:57:20 +0900

    main

--- Hotel/test.py
+++ Hotel/test.py
@@ -1,3 +1,16 @@
-import json
-from datetime import datetime
+# app.py
+from flask import Flask, render_template, url_for
 
+app = Flask(__name__)
+@app.route('/')
+
+@app.route('/index.html')
+def index():
+    return render_template('index.html')
+
+@app.route('/room1.html')
+def access():
+    return render_template('room1.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
