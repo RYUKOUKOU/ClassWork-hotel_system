b7cba7e5e1b0fbf927ccaa0172158c6af0ddd4a3
k023c1020 <k023c1020@g.neec.ac.jp>
2023-12-14 13:41:07 +0900

    Update check_in_out.py

--- Hotel/check_in_out.py
+++ Hotel/check_in_out.py
@@ -1,11 +1,8 @@
-<<<<<<< HEAD
 
+from system import read_info,save_info
 def check(roomind, state, name, phone, timestamp):
     pass
 
 
 if __name__ == '__main__':
     check()
-=======
-from system import read_info,save_info
->>>>>>> a4926251e04bf2505af59a78e099ebe2de6a562a
