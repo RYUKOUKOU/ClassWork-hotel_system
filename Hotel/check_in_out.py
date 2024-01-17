ac1d6a80f3ecfa465bffb9ba9a7539302028c009
k023c1020 <k023c1020@g.neec.ac.jp>
2024-01-17 15:38:13 +0900

    入住退房

--- Hotel/check_in_out.py
+++ Hotel/check_in_out.py
@@ -2,10 +2,24 @@
 from system import read_info,save_info
 
 def check_in(fjh,zy,xm,dh,sj):
-    save_info(fjh,zy,xm,dh,sj)
+    room_data = read_info(fjh)
+    if room_data[0] == 'True':
+        return 'Error1'
+    else:
+        if save_info(fjh,zy,xm,dh,sj):
+            return 'Check in'
+        else:
+            return 'Error2'
 
-def check_out(fjh,zy,xm,dh,sj):
-    save_info(fjh,zy,xm,dh,sj)
+def check_out(fjh):
+    room_data = read_info(fjh)
+    if room_data[0] == 'False':
+        return 'Error3'
+    else:
+        save_info(fjh,'False','','','')
+        return 'Check out'
 
-check_in(1, True, 'AAA', '09012345678', '2023-12-14 18:17:01.501492')
-check_out(1, False, 'None', '0', '0')
+# print(check_in(2, 'True', 'AAA', '09012345678', '2023-12-14 18:17:01.501492'))
+# print(check_out(2))
+# check_in(1, True, 'AAA', '09012345678', '2023-12-14 18:17:01.501492')
+# check_out(1, False, 'None', '0', '0')
