087601c85e32ad684c75054bd468f2368c9e2d16
RYUKOUKOU <liuhonghao159@gmail.com>
2024-05-29 22:12:44 +0900

    Update main.py

--- task_L1/main.py
+++ task_L1/main.py
@@ -2,8 +2,10 @@ import numpy as np
 import random
 import os
 
+H,W=5,7
 com_boat=(2,2,2,3,3)
 bullet=10
+cheat_mode=True
 #初期化
 def initialize(H,W):
     space_csee = np.zeros((H, W))
@@ -34,7 +36,7 @@ def attack(x, y, space_csee, space_attacked):
                 print("打つ！")
                 return True
             else:
-                space_attacked[y,x] = -1
+                space_attacked[y,x] = 2
                 print("ミス！")
                 return False
         else:
@@ -44,14 +46,26 @@ def attack(x, y, space_csee, space_attacked):
         print("弾は残っていない。")
         return False
 
-def refresh_screen(space):#中断点，准备打印画面
-    os.system('cls' if os.name == 'nt' else 'clear')
+def cheat(space_csee):
+    global cheat_mode
+    print(space_csee)
+    print(space_csee[0,2])
 
+    
+def refresh_screen(space1, space2): # 中断点，准备打印画面
+    os.system('cls' if os.name == 'nt' else 'clear')
+    cheat(space1)
+    H, W = space2.shape
+    print("攻撃結果:")
+    print(" |" + "  ".join(str(i) for i in range(W)))
+    print("".join("———" for i in range(W)))
+    for i in range(H):
+        row = [str(int(space2[i, j])) if space2[i, j] != 0 else "." for j in range(W)]#断点
+        print(f"{i} " + "  ".join(row))
 
 def main():
-    space_csee, space_attacked = initialize(5, 5)
+    space_csee, space_attacked = initialize(H,W)
     while bullet > 0:
-        print(space_csee)
         try:
             x = int(input("攻撃用の x 座標を入力してください: "))
             y = int(input("攻撃用の y 座標を入力してください: "))
@@ -59,7 +73,7 @@ def main():
             print(f"残り弾数: {bullet}")
         except ValueError:
             print("無効入力。 整数の座標を入力してください。")
-        print(space_attacked)
+        refresh_screen(space_csee,space_attacked)
 
 if __name__ == "__main__":
     main()
